e40e04eb7d50641d60ac8edc5b59e38822ae7428
Joel Z <joel@jcz.dev>
2026-07-26 02:57:14 -0700

    Turn 10120

--- sandbox/explore_cerulean.py
+++ sandbox/explore_cerulean.py
@@ -1,23 +1,18 @@
 import mgba
-import time
 
-print("Current pos:", mgba.get_coordinates())
+print("Start pos:", mgba.get_coordinates())
 
-# Let's explore East along row 18 from (10,18)
-# Walk 15 steps Right to (25,18)
-mgba.press_buttons(["Right"] * 15)
-pos1 = mgba.get_coordinates()
-print("After 15 Right:", pos1)
+# Move Down to row 20 (2 steps Down)
+mgba.press_buttons(["Down", "Down"])
+print("At row 20:", mgba.get_coordinates())
 mgba.take_screenshot()
 
-# Walk 5 steps Right to (30,18) or as far as possible
-mgba.press_buttons(["Right"] * 5)
-pos2 = mgba.get_coordinates()
-print("After 5 more Right:", pos2)
+# Walk Right 12 steps to see east of Gym
+mgba.press_buttons(["Right"] * 12)
+print("After 12 Right:", mgba.get_coordinates())
 mgba.take_screenshot()
 
-# Try going Up from here
+# Try walking Up 5 steps
 mgba.press_buttons(["Up"] * 5)
-pos3 = mgba.get_coordinates()
-print("After 5 Up:", pos3)
+print("After 5 Up:", mgba.get_coordinates())
 mgba.take_screenshot()
