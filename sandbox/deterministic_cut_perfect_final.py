dcf407a95588fdfa0894276b070372a0ee0bb861
Joel Z <joel@jcz.dev>
2026-07-29 04:51:22 -0700

    Turn 12445

--- sandbox/deterministic_cut_perfect_final.py
+++ sandbox/deterministic_cut_perfect_final.py
@@ -1,21 +1,23 @@
 import mgba
 import time
 
+print("Turning DOWN to face the cuttable bush...")
+mgba.press_buttons(["Down"])
+time.sleep(0.5)
+
 print("Opening Start menu...")
 mgba.press_buttons(["Start"])
-time.sleep(1.0) # Increased delay to be absolutely safe
+time.sleep(1.0)
 
 print("Entering POKéMON menu...")
 mgba.press_buttons(["A"])
 time.sleep(1.0)
 
 print("Selecting TRUFFLE...")
-# Cursor in party menu is at GUSTY (3rd). Press Up once to go to TRUFFLE (2nd).
 mgba.press_buttons(["Up", "sleep 200", "A"])
 time.sleep(1.0)
 
 print("Selecting CUT...")
-# TRUFFLE's sub-menu opens pointing at DIG (1st). Press Down once to go to CUT (2nd).
 mgba.press_buttons(["Down", "sleep 200", "A"])
 time.sleep(2.0)
 
