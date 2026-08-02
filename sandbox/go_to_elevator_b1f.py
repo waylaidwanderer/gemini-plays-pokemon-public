b8176830a64c0bf3bf57e61a9f0d4c83b155f780
Joel Z <joel@jcz.dev>
2026-08-01 22:04:46 -0700

    Turn 15863

--- sandbox/go_to_elevator_b1f.py
+++ sandbox/go_to_elevator_b1f.py
@@ -10,26 +10,13 @@ def move(buttons):
 pos = mgba.get_coordinates()
 print(f"Starting at: {pos}")
 
-# Walk up to Row 5
-# Current position is (20, 14)
-# Let's walk up step by step
-for i in range(14, 5, -1):
-    pos = move(["Up"])
-    if pos['y'] != i - 1:
-        print(f"Blocked at {pos}")
-        break
+# Walk Right 1 step to Column 25
+if pos['x'] == 24 and pos['y'] == 7:
+    pos = move(["Right"])
 
-# If we are at Row 5, walk right to Column 24
-if pos['y'] == 5:
-    for i in range(pos['x'], 24):
-        pos = move(["Right"])
-        if pos['x'] != i + 1:
-            print(f"Blocked at {pos}")
-            break
-
-# If we are at (24, 5), walk down to Row 17
-if pos['x'] == 24 and pos['y'] == 5:
-    for i in range(5, 17):
+# Walk Down Column 25
+if pos['x'] == 25:
+    for i in range(pos['y'], 20):
         pos = move(["Down"])
         if pos['y'] != i + 1:
             print(f"Blocked at {pos}")
