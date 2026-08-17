003e05bb58f782c4224f7fe4b4425777ad70fd1f
Joel Z <joel@jcz.dev>
2026-08-16 22:55:13 -0700

    Turn 43095

--- sandbox/check_bag.py
+++ sandbox/check_bag.py
@@ -1,40 +1,52 @@
 import mgba
 import time
 
-print("--- OPENING MENU AND INSPECTING BAG ---")
+def press_and_wait(button, delay=0.25):
+    mgba.press_buttons([button])
+    time.sleep(delay)
 
-# 1. Open the menu
-mgba.press_buttons(["Start", "sleep 500"])
+def check_bag_perfect():
+    print("Opening start menu...")
+    press_and_wait("Start", 0.5)
+    
+    print("Forcing cursor to top (POKEDEX)...")
+    for _ in range(6):
+        press_and_wait("Up", 0.2)
+        
+    print("Moving to ITEM...")
+    press_and_wait("Down", 0.25)
+    press_and_wait("Down", 0.25)
+    press_and_wait("A", 0.8)
+    
+    # Take screenshot of Page 1
+    p1 = mgba.take_screenshot()
+    print("Bag Page 1:", p1)
+    
+    # Scroll down 4 times
+    print("Scrolling down to Page 2...")
+    for _ in range(4):
+        press_and_wait("Down", 0.2)
+    time.sleep(0.5)
+    
+    # Take screenshot of Page 2
+    p2 = mgba.take_screenshot()
+    print("Bag Page 2:", p2)
+    
+    # Scroll down 4 more times
+    print("Scrolling down to Page 3...")
+    for _ in range(4):
+        press_and_wait("Down", 0.2)
+    time.sleep(0.5)
+    
+    # Take screenshot of Page 3
+    p3 = mgba.take_screenshot()
+    print("Bag Page 3:", p3)
+    
+    # Exit bag and menu
+    print("Exiting bag...")
+    press_and_wait("B", 0.5)
+    print("Exiting start menu...")
+    press_and_wait("B", 0.5)
+    print("Done!")
 
-# 2. Make sure we are at the top (POKEDEX)
-mgba.press_buttons(["Up", "Up", "Up", "Up", "Up", "sleep 200"])
-
-# 3. Move down to ITEM (2 steps down from POKEDEX)
-mgba.press_buttons(["Down", "Down", "sleep 200"])
-
-# 4. Press A to select ITEM
-mgba.press_buttons(["A", "sleep 500"])
-
-# Take screenshot of page 1
-p1 = mgba.take_screenshot()
-print("Bag Page 1:", p1)
-
-# Press Down 4 times to scroll down to see more items
-for i in range(4):
-    mgba.press_buttons(["Down", "sleep 100"])
-time.sleep(0.5)
-
-p2 = mgba.take_screenshot()
-print("Bag Page 2:", p2)
-
-# Press Down 4 more times
-for i in range(4):
-    mgba.press_buttons(["Down", "sleep 100"])
-time.sleep(0.5)
-
-p3 = mgba.take_screenshot()
-print("Bag Page 3:", p3)
-
-# Exit bag and menu
-mgba.press_buttons(["B", "sleep 300", "B", "sleep 300"])
-print("Done!")
+check_bag_perfect()
