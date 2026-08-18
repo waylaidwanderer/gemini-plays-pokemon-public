b50e94364eb03164ed2145545b254192e0f1ef4d
Joel Z <joel@jcz.dev>
2026-08-18 03:22:33 -0700

    Turn 45185

--- sandbox/talk_president.py
+++ sandbox/talk_president.py
@@ -2,13 +2,23 @@ import mgba
 import time
 
 # We start at (6, 9)
-# Move UP to (6, 7)
-print("Moving Up to (6, 8)...")
+# Move Left to (5, 9)
+print("Moving Left to (5, 9)...")
+mgba.press_buttons(["Left"])
+time.sleep(0.3)
+
+# Move UP to (5, 7)
+print("Moving Up to (5, 8)...")
 mgba.press_buttons(["Up"])
 time.sleep(0.3)
 
-print("Moving Up to (6, 7)...")
+print("Moving Up to (5, 7)...")
 mgba.press_buttons(["Up"])
+time.sleep(0.3)
+
+# Move Right to (6, 7)
+print("Moving Right to (6, 7)...")
+mgba.press_buttons(["Right"])
 time.sleep(0.3)
 
 # Face RIGHT and speak
