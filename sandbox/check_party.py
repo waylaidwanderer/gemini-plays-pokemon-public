dc0f537b9e23283bc11cf4f2c298c0a631b6418e
Joel Z <joel@jcz.dev>
2026-08-19 16:47:25 -0700

    Turn 48515

--- sandbox/check_party.py
+++
@@ -1,5 +0,0 @@
-import mgba
-import time
-
-print("Opening START menu and POKéMON menu, then stopping so we can see it...")
-mgba.press_buttons(["Start", "sleep 500", "Down", "sleep 200", "A", "sleep 800"])
