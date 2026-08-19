3e4f2faa771a9e904762a7a20f0343cb338e5153
Joel Z <joel@jcz.dev>
2026-08-19 06:46:45 -0700

    Turn 47608

--- sandbox/walk_to_1f_switch.py
+++ sandbox/walk_to_1f_switch.py
@@ -6,17 +6,13 @@ def run_from_battle():
     mgba.press_buttons(["A", "sleep 500", "A", "sleep 1500", "Right", "Down", "A", "sleep 1500", "A", "sleep 1000"])
 
 path = [
-    # Up to (10, 11)
-    ('Up', 10, 25), ('Up', 10, 24), ('Up', 10, 23), ('Up', 10, 22),
-    ('Up', 10, 21), ('Up', 10, 20), ('Up', 10, 19), ('Up', 10, 18),
-    ('Up', 10, 17), ('Up', 10, 16), ('Up', 10, 15), ('Up', 10, 14),
-    ('Up', 10, 13), ('Up', 10, 12), ('Up', 10, 11),
-    # Right to (18, 11)
-    ('Right', 11, 11), ('Right', 12, 11), ('Right', 13, 11), ('Right', 14, 11),
-    ('Right', 15, 11), ('Right', 16, 11), ('Right', 17, 11), ('Right', 18, 11),
-    # Up to (18, 7)
-    ('Up', 18, 10), ('Up', 18, 9), ('Up', 18, 8), ('Up', 18, 7),
+    # Left to (10, 12)
+    ('Left', 12, 12), ('Left', 11, 12), ('Left', 10, 12),
+    # Up to (10, 7)
+    ('Up', 10, 11), ('Up', 10, 10), ('Up', 10, 9), ('Up', 10, 8), ('Up', 10, 7),
     # Right to (22, 7)
+    ('Right', 11, 7), ('Right', 12, 7), ('Right', 13, 7), ('Right', 14, 7),
+    ('Right', 15, 7), ('Right', 16, 7), ('Right', 17, 7), ('Right', 18, 7),
     ('Right', 19, 7), ('Right', 20, 7), ('Right', 21, 7), ('Right', 22, 7),
     # Up to (22, 1)
     ('Up', 22, 6), ('Up', 22, 5), ('Up', 22, 4), ('Up', 22, 3), ('Up', 22, 2), ('Up', 22, 1),
@@ -24,7 +20,7 @@ path = [
     ('Right', 23, 1)
 ]
 
-print("Starting walk to 1F Mewtwo switch...")
+print("Starting walk to 1F Mewtwo switch (Route 3)...")
 step_index = 0
 while step_index < len(path):
     btn, target_x, target_y = path[step_index]
