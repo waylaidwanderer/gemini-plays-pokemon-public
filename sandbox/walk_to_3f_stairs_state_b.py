71fb72e1d790a45acf41cae5bc4b56fde091501d
Joel Z <joel@jcz.dev>
2026-08-19 16:18:20 -0700

    Turn 48470

--- sandbox/walk_to_3f_stairs_state_b.py
+++ sandbox/walk_to_3f_stairs_state_b.py
@@ -8,31 +8,26 @@ def run_from_battle():
     mgba.press_buttons(["B", "sleep 400", "B", "sleep 400"])
     time.sleep(1.0)
 
-print("Navigating to 3F stairs (18, 2) on 2F in State B via Column 6...")
+print("Navigating directly to 3F stairs (18, 2) on 2F in State B via Row 9...")
 buttons_pressed = 0
 
 while True:
     pos = mgba.get_coordinates()
     print(f"Current Position: {pos}")
     
+    # We warped to 3F (3F landing is at (18, 2) or pos['y'] changes)
     if pos['x'] == 18 and pos['y'] == 2:
         print("At stairs! Toggling warp to 3F...")
         mgba.press_buttons(["Up"])
         break
         
     btn = None
-    if pos['x'] == 8 and pos['y'] > 9:
-        btn = 'Up'
-    elif pos['y'] == 9 and pos['x'] > 6:
-        btn = 'Left'
-    elif pos['x'] == 6 and pos['y'] > 6:
-        btn = 'Up'
-    elif pos['y'] == 6 and pos['x'] < 12:
+    if pos['x'] == 7 and pos['y'] == 8:
+        btn = 'Down'
+    elif pos['y'] == 9 and pos['x'] < 12:
         btn = 'Right'
-    elif pos['x'] == 12 and pos['y'] == 6:
+    elif pos['x'] == 12 and pos['y'] > 5:
         btn = 'Up'
-    elif pos['x'] == 12 and pos['y'] == 5:
-        btn = 'Right'
     elif pos['y'] == 5 and pos['x'] < 18:
         btn = 'Right'
     elif pos['x'] == 18 and pos['y'] > 2:
@@ -46,17 +41,21 @@ while True:
         elif pos['x'] > 18:
             btn = 'Left'
         else:
-            # General coordinate guidance
             if pos['y'] < 5:
                 btn = 'Down'
             else:
-                # We are in the middle, try to reach column 6 or row 9
-                if pos['y'] > 9:
-                    btn = 'Up'
-                elif pos['x'] > 6:
-                    btn = 'Left'
+                if pos['x'] < 12:
+                    if pos['y'] > 9:
+                        btn = 'Up'
+                    elif pos['y'] < 9:
+                        btn = 'Down'
+                    else:
+                        btn = 'Right'
                 else:
-                    btn = 'Right'
+                    if pos['y'] > 5:
+                        btn = 'Up'
+                    else:
+                        btn = 'Right'
                 
     if not btn:
         break
