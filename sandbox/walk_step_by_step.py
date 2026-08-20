bc32d85adbfad227948318844d501cb71744612b
Joel Z <joel@jcz.dev>
2026-08-19 20:58:45 -0700

    Turn 48968

--- sandbox/walk_step_by_step.py
+++ sandbox/walk_step_by_step.py
@@ -1,6 +1,29 @@
 import mgba
 import time
 
+route = [
+    ("Up", 19, 4),
+    ("Up", 19, 3),
+    ("Right", 20, 3),
+    ("Right", 21, 3),
+    ("Right", 22, 3),
+    ("Right", 23, 3),
+    ("Right", 24, 3),
+    ("Right", 25, 3),
+    ("Down", 25, 4),
+    ("Down", 25, 5),
+    ("Down", 25, 6),
+    ("Down", 25, 7),
+    ("Down", 25, 8),
+    ("Down", 25, 9),
+    ("Down", 25, 10),
+    ("Down", 25, 11),
+    ("Down", 25, 12),
+    ("Down", 25, 13),
+    ("Down", 25, 14),
+    ("Left", 24, 14),
+]
+
 def walk_route(path):
     print("Starting step-by-step walk...")
     # Dismiss any text box first
@@ -22,27 +45,4 @@ def walk_route(path):
     return True
 
 if __name__ == "__main__":
-    # We are at (19, 5) on 3F.
-    route = [
-        ("Up", 19, 4),
-        ("Up", 19, 3),
-        ("Right", 20, 3),
-        ("Right", 21, 3),
-        ("Right", 22, 3),
-        ("Right", 23, 3),
-        ("Right", 24, 3),
-        ("Right", 25, 3),
-        ("Down", 25, 4),
-        ("Down", 25, 5),
-        ("Down", 25, 6),
-        ("Down", 25, 7),
-        ("Down", 25, 8),
-        ("Down", 25, 9),
-        ("Down", 25, 10),
-        ("Down", 25, 11),
-        ("Down", 25, 12),
-        ("Down", 25, 13),
-        ("Down", 25, 14),
-        ("Left", 24, 14),
-    ]
     walk_route(route)
