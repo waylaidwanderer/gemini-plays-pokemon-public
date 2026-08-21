311fa02cba79c8e6e2ef8d2cb134fd81c3ee04da
Joel Z <joel@jcz.dev>
2026-08-21 11:19:09 -0700

    Turn 52354

--- sandbox/debug_switch.py
+++ sandbox/debug_switch.py
@@ -1,9 +1,21 @@
 import mgba
 import time
 
-# Let's open the START menu, go to POKéMON, and inspect the screen
-mgba.press_buttons(["Start", "sleep 500", "Down", "A", "sleep 500"])
-time.sleep(1.5)
+print("Dismissing 'Not quite yet!'...")
+mgba.press_buttons(["B"])
+time.sleep(1.0)
 
-screenshot_file = mgba.take_screenshot()
-print(f"Party menu opened. Screenshot saved as {screenshot_file}")
+print("Interacting with switch...")
+mgba.press_buttons(["A"])
+time.sleep(1.2) # wait for text to scroll and Yes/No menu to appear
+
+print("Selecting 'Yes'...")
+mgba.press_buttons(["A"])
+time.sleep(1.2) # wait for "Who wouldn't?" text
+
+print("Dismissing 'Who wouldn't?'...")
+mgba.press_buttons(["B"])
+time.sleep(0.5)
+
+print("Finished!")
+mgba.take_screenshot()
