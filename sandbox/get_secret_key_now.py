fa8f494e39cf5be96493100cea2f4a0889d1bdf7
Joel Z <joel@jcz.dev>
2026-08-24 02:15:09 -0700

    Turn 57806

--- sandbox/get_secret_key_now.py
+++ sandbox/get_secret_key_now.py
@@ -108,7 +108,10 @@ elif y == 11 and x == 5:
 elif (y == 11 or y == 10) and x == 7:
     print("PHASE: Inside 3F West (landing). Walking to switch at (2, 12)...")
     if not walk_to(7, 11): sys.exit(1)
-    if not walk_to(2, 11): sys.exit(1) # Row 11 is completely open from Column 7 to Column 2!
+    if not walk_to(4, 11): sys.exit(1)
+    if not walk_to(4, 13): sys.exit(1)
+    if not walk_to(1, 13): sys.exit(1)
+    if not walk_to(2, 13): sys.exit(1)
     if not walk_to(2, 12): sys.exit(1)
     print("Face UP towards statue switch...")
     mgba.press_buttons(["Up", "sleep 450"])
