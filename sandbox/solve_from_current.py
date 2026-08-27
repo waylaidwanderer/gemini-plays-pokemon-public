a913feeabce054e81b8c0b8d7f10cbdaa357e106
Joel Z <joel@jcz.dev>
2026-08-27 07:10:16 -0700

    Turn 62809

--- sandbox/solve_from_current.py
+++ sandbox/solve_from_current.py
@@ -36,10 +36,9 @@ def run_steps(steps):
 pos = mgba.get_coordinates()
 print("Starting from:", pos)
 
-if pos == {"x": 24, "y": 11}:
+if pos == {"x": 23, "y": 11}:
     print("Walking to B1F East stairs...")
     steps = [
-        ("Left", {"x": 23, "y": 11}),
         ("Up", {"x": 23, "y": 10}),
         ("Up", {"x": 23, "y": 9}),
         ("Up", {"x": 23, "y": 8}),
