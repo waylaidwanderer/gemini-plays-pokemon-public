45e68784357cf0a1b6f6d3ec794f52b9e397dc19
Joel Z <joel@jcz.dev>
2026-08-28 13:55:59 -0700

    Turn 64937

--- sandbox/test_menu.py
+++ sandbox/test_menu.py
@@ -1,34 +1,9 @@
 import mgba
 import time
 
-def test():
-    # Clear any menus
-    mgba.press_buttons(["B", "sleep 200", "B", "sleep 200", "B", "sleep 200"])
-    
-    # Open start menu
-    mgba.press_buttons(["Start", "sleep 500"])
-    
-    # Press Up 10 times to guarantee we are at the top (POKEDEX)
-    for _ in range(10):
-        mgba.press_buttons(["Up", "sleep 100"])
-        
-    # Press Down twice to go to ITEM
-    mgba.press_buttons(["Down", "sleep 200", "Down", "sleep 200"])
-    
-    # Press A to open the bag
-    mgba.press_buttons(["A", "sleep 500"])
-    
-    # Take screenshot of first page of bag
-    mgba.take_screenshot()
-    print("Screenshot of first page of bag taken!")
-    
-    # Scroll down 7 times to see next page of bag
-    for _ in range(7):
-        mgba.press_buttons(["Down", "sleep 100"])
-    mgba.take_screenshot()
-    print("Screenshot of second page of bag taken!")
-    
-    # Close bag and menu
-    mgba.press_buttons(["B", "sleep 300", "Start", "sleep 300"])
+print("Pressing Start...")
+mgba.press_buttons(["Start"])
+time.sleep(1.0)
 
-test()
+img = mgba.take_screenshot()
+print("Menu open screenshot:", img)
