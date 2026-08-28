82aa61e19cd6b3d89a4d5a1cc34ea3f12f9a2645
Joel Z <joel@jcz.dev>
2026-08-28 14:54:57 -0700

    Turn 65032

--- sandbox/probe_gym_local.py
+++ sandbox/probe_gym_local.py
@@ -1,30 +1,37 @@
 import mgba
 import time
+from PIL import Image
 
 def get_pos():
     pos = mgba.get_coordinates()
     return (pos['x'], pos['y'])
 
-print("Starting Giovanni search to the right from current position:", get_pos())
+print("Starting exhaustive search for Giovanni in the upper-right room from current:", get_pos())
 
-# Steps from (14, 3):
-# 1. Right to (15, 3)
-# ...
-# 7. Right to (21, 3)
-steps = []
-for x in range(15, 22):
-    steps.append(("Right", (x, 3)))
-    
-for d, c in steps:
-    mgba.press_buttons(["B"])
-    time.sleep(0.1)
-    
-    old_pos = get_pos()
-    mgba.press_buttons([d])
+# We are at (18, 3)
+# We will walk horizontally along Row 3 from Column 14 to Column 21
+# and print coordinates plus take screenshots to find Giovanni
+
+# 1. Walk Left to (14, 3)
+pos = get_pos()
+while pos[0] > 14:
+    mgba.press_buttons(["Left"])
     time.sleep(0.55)
-    new_pos = get_pos()
-    print(f"Tried {d} from {old_pos}. Landed at: {new_pos}")
-    
-    if new_pos != c:
-        print(f"Offset/block detected! Expected {c}, got {new_pos}. Aborting.")
-        break
+    pos = get_pos()
+    print("Walked Left, position:", pos)
+
+# Take screenshot at (14, 3)
+mgba.take_screenshot()
+
+# 2. Walk Right to (21, 3)
+pos = get_pos()
+while pos[0] < 21:
+    mgba.press_buttons(["Right"])
+    time.sleep(0.55)
+    pos = get_pos()
+    print("Walked Right, position:", pos)
+
+# Take screenshot at (21, 3)
+mgba.take_screenshot()
+
+print("Search complete. Current Position:", get_pos())
