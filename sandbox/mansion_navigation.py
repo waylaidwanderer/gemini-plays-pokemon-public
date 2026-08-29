5e2e71f2562ffaa4713c1e216f1023cf9bb215f7
Joel Z <joel@jcz.dev>
2026-08-29 02:06:17 -0700

    Turn 65909

--- sandbox/mansion_navigation.py
+++ sandbox/mansion_navigation.py
@@ -2,7 +2,7 @@ import mgba
 import time
 
 def walk_path(steps):
-    print("Executing path...")
+    print("Executing master bypass route...")
     for action in steps:
         current_pos = mgba.get_coordinates()
         mgba.press_buttons([action])
@@ -14,11 +14,13 @@ def walk_path(steps):
         print(f"Moved to: {new_pos}")
     return True
 
-# Current position is (4, 13)
+# Current position is (5, 11)
 # We want to walk:
-# Up 2 to (4, 11)
-# Right 8 to (12, 11)
+# Up 2 to (5, 9)
+# Right 4 to (9, 9)
+# Down 2 to (9, 11)
+# Right 3 to (12, 11)
 # Up 2 to (12, 9)
 
-path = ["Up"]*2 + ["Right"]*8 + ["Up"]*2
+path = ["Up"]*2 + ["Right"]*4 + ["Down"]*2 + ["Right"]*3 + ["Up"]*2
 walk_path(path)
