3d1bf80eede44a12ebb6742b6f8c84a3f5d178aa
Joel Z <joel@jcz.dev>
2026-08-29 07:10:07 -0700

    Turn 66423

--- sandbox/test_switch.py
+++ sandbox/test_switch.py
@@ -1,30 +1,21 @@
 import mgba
 import time
-from PIL import Image
 
 def main():
     pos = mgba.get_coordinates()
     print(f"test_switch: Start coordinates: {pos}")
     
     # We are at (1, 10)
-    # Walk Down to (1, 12)
-    mgba.press_buttons(["Down"])
-    time.sleep(0.4)
-    mgba.press_buttons(["Down"])
-    time.sleep(0.4)
-    
-    # Walk Right to (2, 12)
+    # Walk Down to (1, 11)
+    if pos['x'] == 1 and pos['y'] == 10:
+        mgba.press_buttons(["Down"])
+        time.sleep(0.4)
+        
+    # Face Right
     mgba.press_buttons(["Right"])
     time.sleep(0.4)
     
-    pos = mgba.get_coordinates()
-    print(f"Standing at: {pos}")
-    
-    # Face UP
-    mgba.press_buttons(["Up"])
-    time.sleep(0.4)
-    
-    # Press A to interact with Mewtwo statue at (2, 11)
+    # Press A to interact with switch at (2, 11)
     print("Pressing A (1)...")
     mgba.press_buttons(["A"])
     time.sleep(1.0)
