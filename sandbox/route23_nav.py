fb423b981442dded78ee7da3ca4aabecedd1aebd
Joel Z <joel@jcz.dev>
2026-09-06 14:29:29 -0700

    Turn 33190

--- sandbox/route23_nav.py
+++ sandbox/route23_nav.py
@@ -18,9 +18,9 @@ class Route23Segment1:
         return True
 
     def clear_text_or_run(self):
-        # First try clearing dialogue text
+        # Clear dialogue text
         self.press(["A", "sleep 180", "B", "sleep 180", "A", "sleep 180", "B", "sleep 180"])
-        # Then try battle run escape just in case
+        # Battle escape if in wild encounter
         self.press(["Down", "sleep 100", "Right", "sleep 100", "A", "sleep 350", "B", "sleep 150", "B", "sleep 100"])
 
     def step(self, d):
@@ -54,20 +54,15 @@ class Route23Segment1:
         return False
 
     def run(self):
-        # Clear initial Cascade badge text
-        self.clear_text_or_run()
-        print("Starting segment 1 at:", self.get_pos())
-        
+        print("Starting segment 1 from:", self.get_pos())
         wps = [
-            (8, 134),
-            (14, 134),
-            (14, 128),
             (14, 124),
-            (8, 124),
-            (8, 118),
+            (9, 124),
+            (9, 118),
             (10, 118),
             (10, 110),
-            (10, 104)
+            (10, 104),
+            (9, 104)
         ]
         for wx, wy in wps:
             self.walk_to(wx, wy)
